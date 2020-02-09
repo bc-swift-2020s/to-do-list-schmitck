49c73c4cb8ad664dd208c565a378bf8f0dfff0f1
schmitck <schmitck@bc.edu>
2020-02-08 19:15:49 -0500

    Initial Commit

---
+++ ToDo List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDo List
+//
+//  Created by Cooper Schmitz on 2/8/20.
+//  Copyright © 2020 Cooper Schmitz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
